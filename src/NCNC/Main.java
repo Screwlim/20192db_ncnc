b14ebd9adfb48fbfeeb05a655d23b9736e49380b
이승수 <dltmdtn116@gmail.com>
2019-11-09 16:05:17 +0900

    스켈레톤 수정 2

--- src/NCNC/Main.java
+++ src/NCNC/Main.java
@@ -15,7 +15,6 @@ public class Main {
 
 	// user info
 	public static String id = null;
-	public static String pw = null;
 	public static boolean admin = false;
 
 	public static void main(String[] args) {
@@ -52,7 +51,62 @@ public class Main {
 
 			switch (menuNum) {
 			case 1:
-				login();
+				boolean res = login();
+				if (res == true && admin == false) {
+					// menu 1-1 수행
+					System.out.println("1. 회원 정보 관리");
+					System.out.println("2. 차량 관리");
+					System.out.println("3. 거래 내역");
+					System.out.println("4. 돌아가기");
+					System.out.println("\n메뉴를 입력하세요 : ");
+
+					menuNum = scan.nextInt();
+
+					switch (menuNum) {
+					case 1:
+
+						break;
+					case 2:
+
+						break;
+					case 3:
+
+						break;
+					case 4:
+						break;
+					default:
+						System.out.println("잘못된 입력입니다!");
+					}
+				} else if (res == true && admin == true) {
+					// menu 1-2 수헹
+					System.out.println("1. 회원 정보 관리");
+					System.out.println("2. 차량 관리");
+					System.out.println("3. 거래 내역");
+					System.out.println("4. 관리자 기능");
+					System.out.println("5. 돌아가기");
+					System.out.println("\n메뉴를 입력하세요 : ");
+
+					menuNum = scan.nextInt();
+
+					switch (menuNum) {
+					case 1:
+
+						break;
+					case 2:
+
+						break;
+					case 3:
+
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+					default:
+						System.out.println("잘못된 입력입니다!");
+					}
+
+				}
 				break;
 			case 2:
 				signup();
@@ -67,72 +121,113 @@ public class Main {
 		}
 	}
 
-	public static void login() {
+	public static boolean login() {
 		String loginID = null;
 		String loginPW = null;
 		String sql = null;
+		boolean result = false;
+		
 		ResultSet rs = null;
 		Statement stmt = null;
-
+		
 		Account userinfo = new Account();
 		Scanner scan = new Scanner(System.in);
-		
-		stmt = conn.createStatement();
+
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
 		try {
 			System.out.println("[로그인]");
 
 			while (true) {
-
 				System.out.print("아이디를 입력하세요! : ");
 				loginID = scan.nextLine();
 
 				System.out.print("비밀번호를 입력하세요! : ");
 				loginPW = scan.nextLine();
 
-				sql = "select * from Account where id = '" + loginID + "' and password = '" + loginPW + "')";
-				
-				
-				
+				sql = "select * from Account where id = '" + loginID + "' and password = '" + loginPW + "'";
+				// System.out.println(sql);
+
+				rs = stmt.executeQuery(sql);
+
+				while (rs.next()) {
+					Main.id = loginID;
+					result = true;
+					break;
+				}
+				if (result == true) {
+					if (rs.getString(10).equals("T")) {
+						System.out.println("관리자님이 로그인 하셨습니다!\n");
+						Main.admin = true;
+					} else {
+						System.out.println(Main.id + "님이 로그인 하셨습니다!\n");
+						Main.admin = false;
+					}
+					return result;
+				} else {
+					System.out.println("로그인 정보를 다시 입력하세요!");
+				}
 			}
 		} catch (Exception e) {
 			System.err.println("login error : " + e.getMessage());
 			System.exit(1);
 		}
+
+		return false;
 	}
 
 	public static void signup() {
-
+		String sql = null;
 		Account userinfo = new Account();
-
 		Scanner scan = new Scanner(System.in);
-/*
-		// 아이디, 비밀번호 검사용
-		for (int i = 0; i < accounts.size(); i++) {
-			map.put(accounts.get(i).getAccountId(), accounts.get(i).getAccountPW());
-		}
+		StringBuffer sb = new StringBuffer();
+		boolean check = false;
+		
+		ResultSet rs = null;
+		Statement stmt = null;
 
 		try {
-
+			
+			stmt = conn.createStatement();
+			
 			System.out.println("[회원가입]");
 			System.out.println("아래 사항을 차례로 기입하세요.(1~9)");
 			System.out.println("'*'는 필수 입력 사항입니다.");
 
 			while (true) {
 
-				while (true) {
-					System.out.println();
-					System.out.print("1.*아이디 : ");
-					userID = scan.nextLine();
-
-					if (map.containsKey(userID)) {
-						System.out.println("중복된 아이디 입니다!");
-					} else {
-						System.out.println("사용 가능한 아이디 입니다.");
-						break;
-					}
+				System.out.println();
+				System.out.print("1.*아이디 : ");
+				userinfo.setAccountID(scan.nextLine().trim());
+				
+				if(userinfo.getAccountId().equals("")) {
+					System.out.println("필수 입력 사항입니다!");
+					continue;
 				}
-
+			
+				//sql = "select * from Account where id = '" + userinfo.getAccountId() + "'";
+				System.out.println(sql);
+				
+				rs = stmt.executeQuery(sql);
+				
+				while(rs.next()) {
+					check = true;
+				}
+				
+				if (check == true) {
+					System.out.println("중복된 아이디 입니다!");
+					continue;
+				} 
+				else {
+					System.out.println("사용 가능한 아이디 입니다.");
+				}
+				
+				/*
 				System.out.print("2.*비밀번호 : ");
 				userPW = scan.nextLine();
 
@@ -156,7 +251,8 @@ public class Main {
 
 				System.out.print("9.직업 : ");
 				Job = scan.nextLine();
-
+				*/
+				/*
 				try {
 					sql = "INSERT INTO ACCOUNT VALUES ('" + userID + "'" + userPW + "'" + Name + "'" + Phone + "'"
 							+ Email + "'" + Address + "'" + Gender + "'" + Birth + "'" + Job + ")";
@@ -164,6 +260,7 @@ public class Main {
 					System.err.println("insert error : " + e.getMessage());
 					System.exit(1);
 				}
+				*/
 
 				System.out.println("회원가입에 성공하셨습니다!");
 
@@ -172,7 +269,5 @@ public class Main {
 			System.err.println("insert error : " + e.getMessage());
 			System.exit(1);
 		}
-		
-		*/
 	}
 }
