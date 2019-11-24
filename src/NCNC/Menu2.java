d6d1a4836875a1ad003359690908a1b3773a9db1
Simon Lim <kkklim1121@hotmail.com>
2019-11-24 22:04:41 +0900

    Menu2 : 잘못된 입력값, 조회 매물 없을 때 상세보기로 넘어가지 않기

--- src/NCNC/Menu2.java
+++ src/NCNC/Menu2.java
@@ -17,9 +17,9 @@ public class Menu2 {
 		System.out.println("[차량 검색 서비스 입니다.]");
 
 		// 모든 차량의 세부정보가 포함되어있는 car_info뷰 생성
-		String sqlv = "create or replace view car_info as Select distinct v.vehicle_num, v.model_year, c.c_type, m.model_name, d.detail_name, v.price , t.t_type, e.ed_type, clrs.color_name, fus.fuel_type, mk.maker_name, clr.color_id, fu.fuel_id\n" + 
-				"															from ((((((((((VEHICLE v join BLIND_INFO b on v.vehicle_num = b.vnum) join detailed_model d on v.dnum = d.detail_id) join model m on m.model_id = d.mno) join category c on v.ctnum = c.c_id)join transmission t on v.tnum = t.t_id)join engine_displacement e on v.enum = e.ed_id)join color clr on v.cnum  = clr.color_id) join colors clrs on clr.color_id = clrs.color_id)join fuel fu on v.fnum = fu.fuel_id)join fuels fus on fu.fuel_id = fus.fuel_id)join maker mk on m.maker_no = mk.maker_id \n" + 
-				"															where b.order_date is null";
+		String sqlv = "create or replace view car_info as Select distinct v.vehicle_num, v.model_year, c.c_type, m.model_name, d.detail_name, v.price , t.t_type, e.ed_type, clrs.color_name, fus.fuel_type, mk.maker_name, clr.color_id, fu.fuel_id\n"
+				+ "															from ((((((((((VEHICLE v join BLIND_INFO b on v.vehicle_num = b.vnum) join detailed_model d on v.dnum = d.detail_id) join model m on m.model_id = d.mno) join category c on v.ctnum = c.c_id)join transmission t on v.tnum = t.t_id)join engine_displacement e on v.enum = e.ed_id)join color clr on v.cnum  = clr.color_id) join colors clrs on clr.color_id = clrs.color_id)join fuel fu on v.fnum = fu.fuel_id)join fuels fus on fu.fuel_id = fus.fuel_id)join maker mk on m.maker_no = mk.maker_id \n"
+				+ "															where b.order_date is null";
 
 		try {
 			stmt = Main.conn.createStatement();
@@ -106,24 +106,24 @@ public class Menu2 {
 
 			if (count == 0) {
 				System.out.println("\n현재 구매 가능한 차량이 없습니다.");
+			} else {
+				// 상세정보 보기
+				while (true) {
+					System.out.println("상세히 보기를 원하시면 y, 메뉴로 돌아가려면 다른 아무 키나 입력하세요. : ");
+					String Vnum = scan.nextLine();
+
+					if (Vnum.equals("y")) {
+						System.out.println("상세보기를 원하는 매물 번호를 입력하세요 : ");
+						Vnum = scan.nextLine();
+						show_selected(Vnum);
+					} else {
+						break;
+					}
+				}
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-
-		// 상세정보 보기
-		while (true) {
-			System.out.println("상세히 보기를 원하시면 y, 메뉴로 돌아가려면 다른 아무 키나 입력하세요. : ");
-			String Vnum = scan.nextLine();
-
-			if (Vnum.equals("y")) {
-				System.out.println("상세보기를 원하는 매물 번호를 입력하세요 : ");
-				Vnum = scan.nextLine();
-				show_selected(Vnum);
-			} else {
-				break;
-			}
 		}
 	}
 
@@ -155,18 +155,16 @@ public class Menu2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		menuNum = scan.nextInt();
 		temp = scan.nextLine();
-		
+
 		menuNum -= 1; // 메뉴 번호와 maker_id와 맞추기 위해 조정함
 
 		// 해당 maker검색
 		String sql = "Select distinct v.vehicle_num, v.model_year, c.c_type, m.model_name, d.detail_name, v.price\n"
 				+ "	from ((((VEHICLE v join BLIND_INFO b on v.vehicle_num = b.vnum) join detailed_model d on v.dnum = d.detail_id) join model m on m.model_id = d.mno) join category c on v.ctnum = c.c_id) \n"
-				+ "	where b.order_date is null \n"
-				+ " and m.maker_no = " + menuNum + "\n"
-				+ "	order by v.price";
+				+ "	where b.order_date is null \n" + " and m.maker_no = " + menuNum + "\n" + "	order by v.price";
 
 		try {
 			rs = stmt.executeQuery(sql);
@@ -174,7 +172,7 @@ public class Menu2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("번호       차량번호       연식                         차종           모델명             가격");
 		try {
 			count = 0;
@@ -230,224 +228,239 @@ public class Menu2 {
 				+ "5. Coupe\n" + "6. Anything");
 		String category = "";
 
-		menuNum = scan.nextInt();
-		temp = scan.nextLine();// buffer flush
+		while (true) {
+			menuNum = scan.nextInt();
+			temp = scan.nextLine();// buffer flush
 
-		switch (menuNum) {
-		case 1:
-			category = "where c.c_type = 'Light-Weight'\n";
-			break;
-		case 2:
-			category = "where c.c_type = 'MidSize'\n";
-			break;
-		case 3:
-			category = "where c.c_type = 'FullSize'\n";
-			break;
-		case 4:
-			category = "where c.c_type = 'SUV'\n";
-			break;
-		case 5:
-			category = "where c.c_type = 'Coupe'\n";
-			break;
-		default:
-			category = "where c.c_type is not null\n";
-			break;
+			if (menuNum == 1) {
+				category = "where c.c_type = 'Light-Weight'\n";
+				break;
+			} else if (menuNum == 2) {
+				category = "where c.c_type = 'MidSize'\n";
+				break;
+			} else if (menuNum == 3) {
+				category = "where c.c_type = 'FullSize'\n";
+				break;
+			} else if (menuNum == 4) {
+				category = "where c.c_type = 'SUV'\n";
+				break;
+			} else if (menuNum == 5) {
+				category = "where c.c_type = 'Coupe'\n";
+				break;
+			} else if (menuNum == 6) {
+				category = "where c.c_type is not null\n";
+				break;
+			} else {
+				System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+			}
 		}
 
-		
-		
 		// 제조사 선택
 		String maker = "";
 
-		System.out.println("제조사를 선택하세요.\n1. Hyundai\n2. Kia\n" + "3. Chevrolet\n" + "4. Ssangyong\n"
-				+ "5. Renault Samsung\n" + "6. Benz\n" + "7. BMW\n" + "8. Audi\n" + "9. Porsche\n"
-				+ "10. Lamborghini\n11. Anything");
+		System.out.println(
+				"제조사를 선택하세요.\n1. Hyundai\n2. Kia\n" + "3. Chevrolet\n" + "4. Ssangyong\n" + "5. Renault Samsung\n"
+						+ "6. Benz\n" + "7. BMW\n" + "8. Audi\n" + "9. Porsche\n" + "10. Lamborghini\n11. Anything");
 
-		menuNum = scan.nextInt();
-		temp = scan.nextLine();// buffer flush
-
-		switch (menuNum) {
-		case 1:
-			maker = "and c.maker_name = 'Hyundai'\n";
-			break;
-		case 2:
-			maker = "and c.maker_name = 'Kia'\n";
-			break;
-		case 3:
-			maker = "and c.maker_name = 'Chevrolet'\n";
-			break;
-		case 4:
-			maker = "and c.maker_name = 'Ssangyong'\n";
-			break;
-		case 5:
-			maker = "and c.maker_name = 'Renault Samsung'\n";
-			break;
-		case 6:
-			maker = "and c.maker_name = 'Benz'\n";
-			break;
-		case 7:
-			maker = "and c.maker_name = 'BMW'\n";
-			break;
-		case 8:
-			maker = "and c.maker_name = 'Audi'\n";
-			break;
-		case 9:
-			maker = "and c.maker_name = 'Porsche'\n";
-			break;
-		case 10:
-			maker = "and c.maker_name = 'Lamborghini'\n";
-		default:
-			maker = "";
-			break;
+		while (true) {
+			menuNum = scan.nextInt();
+			temp = scan.nextLine();// buffer flush
+			if (menuNum == 1) {
+				maker = "and c.maker_name = 'Hyundai'\n";
+				break;
+			} else if (menuNum == 2) {
+				maker = "and c.maker_name = 'Kia'\n";
+				break;
+			} else if (menuNum == 3) {
+				maker = "and c.maker_name = 'Chevrolet'\n";
+				break;
+			} else if (menuNum == 4) {
+				maker = "and c.maker_name = 'Ssangyong'\n";
+				break;
+			} else if (menuNum == 5) {
+				maker = "and c.maker_name = 'Renault Samsung'\n";
+				break;
+			} else if (menuNum == 6) {
+				maker = "and c.maker_name = 'Benz'\n";
+				break;
+			} else if (menuNum == 7) {
+				maker = "and c.maker_name = 'BMW'\n";
+				break;
+			} else if (menuNum == 8) {
+				maker = "and c.maker_name = 'Audi'\n";
+				break;
+			} else if (menuNum == 9) {
+				maker = "and c.maker_name = 'Porsche'\n";
+				break;
+			} else if (menuNum == 10) {
+				maker = "and c.maker_name = 'Lamborghini'\n";
+				break;
+			} else if (menuNum == 11) {
+				maker = "";
+				break;
+			} else {
+				System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+			}
 		}
 
 		System.out.println("사용 연료를 선택하여 주세요.\n1. Gasoline\n" + "2. Diesel\n" + "3. LPG\n" + "4. Electric\n"
 				+ "5. Gasoline and Electric\n" + "6. Gasoline and LPG\n7. Anything");
-		menuNum = scan.nextInt();
-		temp = scan.nextLine();// buffer flush
 		String fuel = "";
-		switch (menuNum) {
-		case 1:
-			fuel = "and c.fuel_id='0'\n";
-			break;
-		case 2:
-			fuel = "and c.fuel_id='1'\n";
-			break;
-		case 3:
-			fuel = "and c.fuel_id='2'\n";
-			break;
-		case 4:
-			fuel = "and c.fuel_id='3'\n";
-			break;
-		case 5:
-			fuel = "and c.fuel_id='4'\n";
-			break;
-		case 6:
-			fuel = "and c.fuel_id='6'\n";
-			break;
-		default:
-			fuel = "";
-			break;
+
+		while (true) {
+			menuNum = scan.nextInt();
+			temp = scan.nextLine();// buffer flush
+			if (menuNum == 1) {
+				fuel = "and c.fuel_id='0'\n";
+				break;
+			} else if (menuNum == 2) {
+				fuel = "and c.fuel_id='1'\n";
+				break;
+			} else if (menuNum == 3) {
+				fuel = "and c.fuel_id='2'\n";
+				break;
+			} else if (menuNum == 4) {
+				fuel = "and c.fuel_id='3'\n";
+				break;
+			} else if (menuNum == 5) {
+				fuel = "and c.fuel_id='4'\n";
+				break;
+			} else if (menuNum == 6) {
+				fuel = "and c.fuel_id='6'\n";
+				break;
+			} else if (menuNum == 7) {
+				fuel = "";
+				break;
+			} else {
+				System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+			}
 		}
-		
-		//변속기 선택 
-		System.out.println("변속기를 선택하여 주세요.\n1. Auto\n" + 
-				"2. Manual\n" + 
-				"3. Semi-Auto\n4. Anything");
-		menuNum = scan.nextInt();
-		temp = scan.nextLine();// buffer flush
-		String transmission = "";
-		switch (menuNum) {
-		case 1:
-			transmission = "and c.t_type ='Auto'\n";
-			break;
-		case 2:
-			transmission = "and c.t_type ='Manual'\n";
-			break;
-		case 3:
-			transmission = "and c.t_type ='Semi-Auto'\n";
-			break;
-		default:
-			transmission = "";
-			break;
+
+		// 변속기 선택
+		System.out.println("변속기를 선택하여 주세요.\n1. Auto\n" + "2. Manual\n" + "3. Semi-Auto\n4. Anything");
+
+		while (true) {
+			menuNum = scan.nextInt();
+			temp = scan.nextLine();// buffer flush
+			String transmission = "";
+			if (menuNum == 1) {
+				transmission = "and c.t_type ='Auto'\n";
+				break;
+			} else if (menuNum == 2) {
+				transmission = "and c.t_type ='Manual'\n";
+				break;
+			} else if (menuNum == 3) {
+				transmission = "and c.t_type ='Semi-Auto'\n";
+				break;
+			} else if (menuNum == 4) {
+				transmission = "";
+				break;
+			} else {
+				System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+			}
 		}
-		
-		int edmin=0;
-		int edmax=1;
+
+		int edmin = 0;
+		int edmax = 1;
 		System.out.println("배기량 조건 설정입니다. 다음 값들을 입력해주세요.(설정하지 않으려면 최소값에 -1을 입력하세요.)");
 		String ed = "";
-		//배기량 선택
-		while(true) {
+		// 배기량 선택
+		while (true) {
 			System.out.print("최소값 :");
 			edmin = scan.nextInt();
 			if (edmin == -1) {
 				ed = "";
 				break;
 			}
-			
+
 			System.out.print("최대값 :");
 			edmax = scan.nextInt();
-			
+
 			if (edmin > edmax) {
 				System.out.println("유효하지 않은 설정 값입니다. 다시 입력해 주세요.");
-			}else {
+			} else {
 				ed = "\nand c.ed_type > " + edmin + " and c.ed_type < " + edmax;
 				break;
 			}
 		}
-		
-		
-		//색상 선택
-		System.out.println("차량의 색상을 선택하여 주세요.\n1. Black\n2. White\n3. Gray\n4. Red\n5. Blue\n6. Black & Gray\n7. White & Gray\n8. Anything");
-		menuNum = scan.nextInt();
-		temp = scan.nextLine();// buffer flush
+
+		// 색상 선택
+		System.out.println(
+				"차량의 색상을 선택하여 주세요.\n1. Black\n2. White\n3. Gray\n4. Red\n5. Blue\n6. Black & Gray\n7. White & Gray\n8. Anything");
 		String color = "";
-		switch (menuNum) {
-		case 1:
-			color = " and c.color_id='0'\n";
-			break;
-		case 2:
-			color = " and c.color_id='1'\n";
-			break;
-		case 3:
-			color = " and c.color_id='2'\n";
-			break;
-		case 4:
-			color = " and c.color_id='3'\n";
-			break;
-		case 5:
-			color = " and c.color_id='4'\n";
-			break;
-		case 6:
-			color = " and c.color_id='5'\n";
-			break;
-		case 7:
-			color = " and c.color_id='6'\n";
-		default:
-			color = "";
-			break;
+
+		while (true) {
+			menuNum = scan.nextInt();
+			temp = scan.nextLine();// buffer flush
+			if (menuNum == 1) {
+				color = " and c.color_id='0'\n";
+				break;
+			} else if (menuNum == 2) {
+				color = " and c.color_id='1'\n";
+				break;
+			} else if (menuNum == 3) {
+				color = " and c.color_id='2'\n";
+				break;
+			} else if (menuNum == 4) {
+				color = " and c.color_id='3'\n";
+				break;
+			} else if (menuNum == 5) {
+				color = " and c.color_id='4'\n";
+				break;
+			} else if (menuNum == 6) {
+				color = " and c.color_id='5'\n";
+				break;
+			} else if (menuNum == 7) {
+				color = " and c.color_id='6'\n";
+				break;
+			} else if (menuNum == 8) {
+				color = "";
+				break;
+			} else {
+				System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+			}
 		}
-		
-		//가격 검색
+
+		// 가격 검색
 		int pmin;
 		int pmax;
 		System.out.println("가격대 조건 설정입니다. 다음 값들을 입력해주세요.(설정하지 않으려면 최소값에 -1을 입력하세요.)");
-		//배기량 선택
+		// 배기량 선택
 		String price;
-		while(true) {
+		while (true) {
 			System.out.print("최소값 :");
 			pmin = scan.nextInt();
-			if(pmin == -1) {
+			if (pmin == -1) {
 				price = "";
 				break;
 			}
 			System.out.print("최대값 :");
 			pmax = scan.nextInt();
-			
+
 			if (pmin > pmax) {
 				System.out.println("유효하지 않은 설정 값입니다. 다시 입력해 주세요.");
-			}else {
+			} else {
 				price = "\nand c.price > " + pmin + " and c.price < " + pmax;
 				break;
 			}
 		}
-		
-		temp = scan.nextLine();//flush
-		
-		String sql = "Select distinct c.vehicle_num, c.model_year, c.c_type, c.model_name, c.detail_name, c.price\n" + 
-				"from car_info c\n" + category + maker + fuel + ed + color + 
-				"order by c.price";
-				
+
+		temp = scan.nextLine();// flush
+
+		String sql = "Select distinct c.vehicle_num, c.model_year, c.c_type, c.model_name, c.detail_name, c.price\n"
+				+ "from car_info c\n" + category + maker + fuel + ed + color + "order by c.price";
+
 		try {
-			stmt  = Main.conn.createStatement();
-			
+			stmt = Main.conn.createStatement();
+
 			rs = stmt.executeQuery(sql);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		System.out.println("\n조건 검색결과 입니다.\n번호       차량번호       연식                         차종           모델명             가격");
+
+		System.out.println(
+				"\n조건 검색결과 입니다.\n번호       차량번호       연식                         차종           모델명             가격");
 		try {
 			int count = 0;
 			while (rs.next()) {
@@ -485,7 +498,7 @@ public class Menu2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	// 모델 이름으로 검색하여 보여줌
@@ -531,26 +544,25 @@ public class Menu2 {
 
 			if (count == 0) {
 				System.out.println("\n현재 구매 가능한 차량이 없습니다.");
+			} else {
+				// 상세정보 보기
+				while (true) {
+					System.out.println("상세히 보기를 원하시면 y, 메뉴로 돌아가려면 다른 아무 키나 입력하세요. : ");
+					String Vnum = scan.nextLine();
+
+					if (Vnum.equals("y")) {
+						System.out.println("상세보기를 원하는 매물 번호를 입력하세요 : ");
+						Vnum = scan.nextLine();
+						show_selected(Vnum);
+					} else {
+						break;
+					}
+				}
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		// 상세정보 보기
-		while (true) {
-			System.out.println("상세히 보기를 원하시면 y, 메뉴로 돌아가려면 다른 아무 키나 입력하세요. : ");
-			String Vnum = scan.nextLine();
-
-			if (Vnum.equals("y")) {
-				System.out.println("상세보기를 원하는 매물 번호를 입력하세요 : ");
-				Vnum = scan.nextLine();
-				show_selected(Vnum);
-			} else {
-				break;
-			}
-		}
-
 	}
 
 	// 선택한 매물에 대하여 상세정보를 보여줌
@@ -651,10 +663,13 @@ public class Menu2 {
 	// 차량을 구매함
 	public static void buycar(String vnum) {
 		Statement stmt = null;
+		String inDate   = new java.text.SimpleDateFormat("yyyyMMdd").format(new java.util.Date());
 
 		// order_info update
-		String sql = "update order_info\n" + "set order_date = TO_DATE( '" + "2019/11/23 03:00:00"
-				+ "', 'YYYY/MM/DD HH:MI:SS')\n" + "where order_info.vnum = '" + vnum + "'";
+		String sql = "update order_info\n" + "set order_date = TO_DATE( '" + inDate 
+				+ "', 'YYYY/MM/DD')\n" + "where order_info.vnum = '" + vnum + "'";
+		
+		System.out.println("구매 시퀄 : " + sql);
 		try {
 			stmt = Main.conn.createStatement();
 
